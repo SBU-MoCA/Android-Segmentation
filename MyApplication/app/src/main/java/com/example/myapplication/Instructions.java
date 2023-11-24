ca9233f28e2b9745223209fc6ad212f123bf1520
SuvabBaral <bsuvab@yahoo.com>
2023-11-23 23:55:41 -0500

    added vibration and alert logic for inactive user

--- MyApplication/app/src/main/java/com/example/myapplication/Instructions.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Instructions.java
@@ -4,21 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
 import android.content.res.Resources;
-import android.graphics.ImageDecoder;
 import android.media.MediaPlayer;
-import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.content.Intent;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
@@ -35,6 +35,12 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
     private FileWriter fw;
     Context context = this;
     MediaPlayer mp = new MediaPlayer();
+
+    // for time specific alerts.
+    private Handler handler;
+    private Runnable showDialogRunnable;
+    private Vibrator vibrator;
+
 
 
     @Override
@@ -72,6 +78,8 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
         JSONArray activityInstructions = null;
         String audioFilename = null;
         String gifImageName = null;
+        Boolean alertUser = false;
+        Integer alertAfterSeconds = 0;
         try {
            jsonObject = new JSONObject(intent.getStringExtra("jsonData"));
         } catch (JSONException e) {
@@ -83,6 +91,8 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
             activityInstructions = activityDetails.getJSONArray("instructions");
             audioFilename = activityDetails.getString("audioFileName");
             gifImageName = activityDetails.getString("gifFileName");
+            alertUser = activityDetails.getBoolean("alertUser");
+            alertAfterSeconds = activityDetails.getInt("alertAfter");
         } catch (JSONException e) {
             System.out.println("CODE ERROR: while getting activity JSON data: " + e);
             e.printStackTrace();
@@ -159,13 +169,20 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
         // log the starting activity time
         // TODO: Add a buffer time for subject to comprehend the instruction.
         logActivityTimings(fw, mAppendFileWriter, "start");
+
+        // initialize required values if we need to alert user after x seconds
+        if (alertUser) {
+            // Initialize handler and runnable
+            initializeHandlerAndRunnable(alertAfterSeconds);
+        }
+
     }
 
     public String getFileNameFormat(String fileLocation, String subjectId) {
         return  fileLocation + '_' + subjectId + ".txt";
     }
 
-    public void logActivityTimings(FileWriter fileWriter, JavaAppendFileWriter fileAppendWriter, String flag) {
+    private void logActivityTimings(FileWriter fileWriter, JavaAppendFileWriter fileAppendWriter, String flag) {
         TimeZone timeZone = TimeZone.getTimeZone("UTC");
         Calendar calendar = Calendar.getInstance(timeZone);
         SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.US);
@@ -179,21 +196,8 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
 
     }
 
-    public void openDialog(
-            String activity, String subjectId, String jsonObject, String activityId, String fileLocation
-    ) {
-        CustomDialog dialog = new CustomDialog(
-                activity,
-                subjectId,
-                jsonObject,
-                activityId,
-                fileLocation
-                );
-        dialog.show(getSupportFragmentManager(), "customDialog");
-    }
-
     // onClick for activityCompleteButton
-    public void openNextInstructionActivity(String subjectId, String jsonObject, String activityId, FileWriter fw, JavaAppendFileWriter mAppendFileWriter, String fileLocation) {
+    private void openNextInstructionActivity(String subjectId, String jsonObject, String activityId, FileWriter fw, JavaAppendFileWriter mAppendFileWriter, String fileLocation) {
         logActivityTimings(fw, mAppendFileWriter, "stop");
         Intent intent = new Intent(this, Instructions.class);
         intent.putExtra("subjectId", subjectId);
@@ -204,7 +208,7 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
     }
 
     // onClick for restartActivityButton
-    public void restartCurrentActivity(String subjectId, String jsonObject, String activityId, String fileLocation) {
+    private void restartCurrentActivity(String subjectId, String jsonObject, String activityId, String fileLocation) {
         System.out.println("Restarting activity: " + activityId);
         JavaAppendFileWriter.removeLastEntryFromFile(getFileNameFormat(fileLocation, subjectId));
         Intent intent = new Intent(this, Instructions.class);
@@ -217,7 +221,7 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
     }
 
     // onClick for startOverActivity
-    public void startOverFromStart(String subjectId, String jsonObject, String activityId, String fileLocation) throws IOException {
+    private void startOverFromStart(String subjectId, String jsonObject, String activityId, String fileLocation) throws IOException {
         JavaAppendFileWriter.truncateFile(getFileNameFormat(fileLocation, subjectId));
         Intent intent = new Intent(this, Instructions.class);
         intent.putExtra("subjectId", subjectId);
@@ -236,6 +240,64 @@ public class Instructions extends AppCompatActivity implements CustomDialog.Cust
         } else {
             startOverFromStart(subjectId, jsonObject, "1", fileLocation);
         }
+    }
+
+    public void openDialog(
+            String activity, String subjectId, String jsonObject, String activityId, String fileLocation
+    ) {
+        CustomDialog dialog = new CustomDialog(
+                activity,
+                subjectId,
+                jsonObject,
+                activityId,
+                fileLocation
+        );
+        dialog.show(getSupportFragmentManager(), "customDialog");
+    }
+
+    private void startTimer(Integer timeoutSeconds) {
+        long timeoutInSeconds = timeoutSeconds * 1000;
+        // Post the runnable to the handler after TIMEOUT_MS
+        handler.postDelayed(showDialogRunnable, timeoutInSeconds);
+    }
+
+    private void resetTimer(Integer timeoutSeconds) {
+        // Remove any existing callbacks and post the runnable again
+        handler.removeCallbacks(showDialogRunnable);
+        startTimer(timeoutSeconds);
+    }
+    private void vibrate() {
+        vibrator.vibrate(500);
+    }
+
+    public void initializeHandlerAndRunnable(Integer timeoutSeconds) {
+        handler = new Handler();
+        vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        showDialogRunnable = new Runnable() {
+            @Override
+            public void run() {
+                // Show the dialog when the user has been inactive for x seconds
+                alertTimeoutDialog();
+                vibrate();
+            }
+        };
+        // Set up touch listener to reset the timer on user interaction
+        View rootView = findViewById(android.R.id.content);
+        rootView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                // Reset the timer on user interaction
+                resetTimer(timeoutSeconds);
+                return true;
+            }
+        });
+        // Start the timer when the activity is created
+        startTimer(timeoutSeconds);
+    }
+
+    public void alertTimeoutDialog() {
+        InactiveAlertDialog timeoutDialogReminder =  new InactiveAlertDialog();
+        timeoutDialogReminder.show(getSupportFragmentManager(), "inactiveDialogReminder");
     }
 
 }
